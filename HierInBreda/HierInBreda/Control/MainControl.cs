0e3e18c683ccce601f2699ed91d43cb9bd51a963
unknown <arrb713@hotmail.com>
2013-12-16 11:46:55 +0100

    Added popup message

--- HierInBreda/HierInBreda/Control/MainControl.cs
+++ HierInBreda/HierInBreda/Control/MainControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 namespace HierInBreda.Control
 {
@@ -16,6 +17,14 @@ namespace HierInBreda.Control
         public MainControl()
         {
             LanguageControl.GetInstance();
+            //showMessage("hello");
+        }
+
+        public static async void showMessage(string msg)
+        {
+            var msgDlg = new Windows.UI.Popups.MessageDialog(msg);
+            msgDlg.DefaultCommandIndex = 1;
+            await msgDlg.ShowAsync();
         }
     }
 }
