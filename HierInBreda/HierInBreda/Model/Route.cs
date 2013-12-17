21779dc15368bc1de3be34ab6fed9d12f2dfef06
imp <johannes.san@gmail.com>
2013-12-17 11:47:26 +0100

    implemented the Route model class
    Signed-off-by: imp <johannes.san@gmail.com>

--- HierInBreda/HierInBreda/Model/Route.cs
+++ HierInBreda/HierInBreda/Model/Route.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HierInBreda.Control;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,38 @@ using System.Threading.Tasks;
 namespace HierInBreda.Model
 {
     /*
-     * @author:
-     * @version:
-     * @description:
+     * @author: Johannes [imp] Michel
+     * @version: 1.0
+     * @description: Model class Route containing a the sight IDs and wether they're yet visited or not.
      */
-    class Route
+    public class Route
     {
+        private Dictionary<Int32, Boolean> visited;
+
+        public Route(DataControl dc)
+        {
+            List<Sight> list = dc.getSightDutch();
+
+            foreach(Sight sight in list)
+            {
+                visited.Add(sight.id, false);
+            }
+        }
+
+        /// <summary>
+        /// Returns false if the sight hasn't been visited and true if it was visited before.
+        /// </summary>
+        public bool isVisited(int ID)
+        {
+            return visited[ID];
+        }
+
+        /// <summary>
+        /// Sets a sight ID to the boolean specified.
+        /// </summary>
+        public void setVisited(int ID, Boolean seen)
+        {
+            visited[ID] = seen;
+        }
     }
 }
